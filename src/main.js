dd6827a8da6487dbafe83cbef0f8b486c8a5572e
Skulkrone <40754599+Skulkrone@users.noreply.github.com>
2022-12-12 16:54:47 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// pour requête http via appel api : utilisation de Axios
+// https://www.npmjs.com/package/vue-axios
+
+createApp(App)
+.use(VueAxios, axios)
+.mount('#app')
+
